464aaae38dd4f647075bcea88580dbd8fddd113a
deveaswj <85638505+deveaswj@users.noreply.github.com>
2024-11-14 12:45:39 -0600

    Tile prefab and script

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,6 +8,9 @@ public class Tile : MonoBehaviour
 
     [SerializeField] float hoverAmount = 0.1f;
     [SerializeField] LayerMask obstacleLayer;
+    [SerializeField] Color hoverColor;
+    [SerializeField] Color selectedColor;
+    private Color defaultColor;
 
     private Vector3 defaultScale;
 
@@ -16,22 +19,19 @@ public class Tile : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         defaultScale = transform.localScale;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        defaultColor = spriteRenderer.color;
     }
 
     void OnMouseEnter()
     {
         transform.localScale += Vector3.one * hoverAmount;
+        spriteRenderer.color = hoverColor;
     }
 
     void OnMouseExit()
     {
         transform.localScale = defaultScale;
+        spriteRenderer.color = defaultColor;
     }
 
     public bool IsWalkable()
